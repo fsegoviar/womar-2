a01f9a958f136932a3f5599b140aafdfa1c0a15f
Felipe Segovia R <fsegovia@lisit.cl>
2023-02-14 09:00:12 -0300

    Se agrega efecto del login

--- src/components/DialogBase.tsx
+++ src/components/DialogBase.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { LoginWithFacebook } from './navbar/login/components/LoginWithFacebook';
 import { LoginWithGoogle } from './navbar/login/components/LoginWithGoogle';
 import { TypeUser } from '../interfaces/Login';
@@ -6,6 +6,7 @@ import axios from 'axios';
 import { FailResponse } from '@greatsumini/react-facebook-login';
 import { Box, Fade, Typography } from '@mui/material';
 import { StandardLogin } from './navbar/login/components/StandardLogin';
+import './styles-login.css';
 
 interface PropsLogin {
   open: boolean;
@@ -16,10 +17,30 @@ interface PropsLogin {
 }
 
 export const DialogBase = (props: PropsLogin) => {
+  const modalRef = useRef<HTMLDivElement>(null!);
+  const containerRef = useRef<HTMLDivElement>(null!);
   // const [open, setOpen] = useState(props.open);
   const [error, setError] = useState(false);
   // const [loading, setLoading] = useState(false);
   const [msgError, setMsgError] = useState('');
+
+  useEffect(() => {
+    if (props.open) modalRef.current.style.display = 'flex';
+  }, [props.open]);
+
+  const closeModal = (): void => {
+    containerRef.current.classList.add('close');
+    setTimeout(() => {
+      containerRef.current.classList.remove('close');
+      modalRef.current.style.display = 'none';
+      props.handleClose();
+    }, 1000);
+  };
+
+  window.addEventListener(
+    'click',
+    (e: any) => e.target === modalRef.current && closeModal()
+  );
 
   const accessLogin = ({
     accessToken = '',
@@ -68,52 +89,49 @@ export const DialogBase = (props: PropsLogin) => {
 
   return (
     <>
-      {/* Fondo blanco */}
-      <div className="fixed top-0 left-0 w-full h-screen bg-white z-50 opacity-75"></div>
       {/* Contendor Dialog */}
       <div
-        className="fixed top-0 left-0 w-full h-screen flex z-50 justify-center items-center"
-        onClick={() => {
-          console.log('Padre');
-        }}
+        className="window-background-login"
+        id="window-background-login"
+        ref={modalRef}
       >
         <div
-          className="relative w-7/12 h-3/6 bg-white flex"
-          style={{ borderRadius: '70px' }}
-          onClick={() => {
-            console.log('Hijo');
-          }}
+          className="window-container-login"
+          id="window-container-login"
+          ref={containerRef}
         >
-          <div
-            className="w-9/12 h-full bg-no-repeat bg-center bg-cover flex justify-end"
-            style={{
-              backgroundImage: `url(${require('../assets/images/img-login.png')})`,
-              borderRadius: '70px 0 0 70px'
-            }}
-          ></div>
-          <div
-            className="absolute top-0 right-0 w-5/12 h-full border-2 border-[#000aff] bg-white p-10"
-            style={{ borderRadius: '70px' }}
-          >
-            <p>Ingresa</p>
-            <StandardLogin onSubmit={accessLogin} />
-            {error && (
-              <Fade in={error}>
-                <Box className="flex justify-center items-center pt-2">
-                  <Typography variant="body1" color="red">
-                    {msgError}
-                  </Typography>
-                </Box>
-              </Fade>
-            )}
+          <div>
+            <div
+              className="w-9/12 h-[450px] bg-no-repeat bg-center bg-cover flex justify-end"
+              style={{
+                backgroundImage: `url(${require('../assets/images/img-login.png')})`,
+                borderRadius: '70px 0 0 70px'
+              }}
+            ></div>
+            <div
+              className="absolute top-0 right-0 w-5/12 h-full border-2 border-[#000aff] bg-white p-10"
+              style={{ borderRadius: '70px' }}
+            >
+              <p>Ingresa</p>
+              <StandardLogin onSubmit={accessLogin} />
+              {error && (
+                <Fade in={error}>
+                  <Box className="flex justify-center items-center pt-2">
+                    <Typography variant="body1" color="red">
+                      {msgError}
+                    </Typography>
+                  </Box>
+                </Fade>
+              )}
 
-            <hr className="mt-3" />
-            <div className="grid gap-4">
-              <LoginWithGoogle response={accessLogin} />
-              <LoginWithFacebook
-                success={accessLogin}
-                failure={loginFacebookFailure}
-              />
+              <hr className="mt-3" />
+              <div className="grid gap-4">
+                <LoginWithGoogle response={accessLogin} />
+                <LoginWithFacebook
+                  success={accessLogin}
+                  failure={loginFacebookFailure}
+                />
+              </div>
             </div>
           </div>
         </div>
